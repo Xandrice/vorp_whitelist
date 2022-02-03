3d366ceec8bfc375b0b8c19df542aefc1a5bd356
Xandrice <48589989+Xandrice@users.noreply.github.com>
2022-02-03 15:28:55 -0800

    Added roles and guild ID for whitelist

--- config.js
+++ config.js
@@ -3,22 +3,21 @@ const config = {
     clientSecret: "",
     token: "",
 
-    guild: "",
+    guild: "921617122055049216",
     roles: [
-        "", // Staff 
-        ""  // Other 
+        "921617122055049220"  // Townfolk 
     ],
 
     httpPort: 7790,
 
     redirectUri: "https://youripserver/discord",
 
-    checkRole: false,
+    checkRole: true,
     
     /*
     Set to true if you use connectQueue and follow the following instructions :
     In connectqueue/shared/sh_queue.lua, on line 474, after deferrals.done(), add :
         TriggerEvent('discordwl:connect', src)
     */
-    connectQueue: false,
+    connectQueue: true,
 };
